9ab2cd23992d2f5337686bb5a72617c2d5059bad
decodload <decodload@gmail.com>
2024-03-09 03:58:58 +0200

    [refactor] hw_L26: task 2. created function 'startApp'

--- lesson_26/HW_L26/scripts/task_2.js
+++ lesson_26/HW_L26/scripts/task_2.js
@@ -4,6 +4,9 @@ function getEvenNumArray(numArray) {
     return numArray.filter((element) => (element % 2) === 0);
 }
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const evenNumArray = getEvenNumArray(numbers);
-console.log(evenNumArray);
+function startApp() {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const evenNumArray = getEvenNumArray(numbers);
+    console.log(evenNumArray);
+}
+startApp();
