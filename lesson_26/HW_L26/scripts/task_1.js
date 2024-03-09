0a47b918a6bbe5a08a6b52956297b101144526f4
decodload <decodload@gmail.com>
2024-03-09 02:29:06 +0200

    [feature] hw_L26: finished task 1

---
+++ lesson_26/HW_L26/scripts/task_1.js
@@ -0,0 +1,24 @@
+// task 1
+
+function getBookTitle(author, booksArray) {
+    const bookObj = booksArray.find((element) => element.author === author);
+    if (bookObj) {
+        return bookObj.title;
+    }
+    return '';
+}
+
+const books = [
+    { title: 'Гаррі Поттер', author: 'Дж.К. Ролінг' },
+    { title: '1984', author: 'Джордж Орвелл' },
+    { title: 'Хоббіт', author: 'Дж.Р.Р. Толкієн' }
+];
+
+const targetValue = 'Джордж Орвелл';
+const bookTitle = getBookTitle(targetValue, books);
+
+if (bookTitle) {
+    console.log(bookTitle);
+} else {
+    console.log('Автор не найден в БД');
+}
