cf686b3944577975402f5c7e7a781b5dbdc5c195
decodload <decodload@gmail.com>
2024-04-08 13:59:51 +0300

    [feature] hw_L34: added event listener into wrapper with recursion. 'hw_2 Gallery'

--- lesson_34/HW_L34/scripts/task_2.js
+++ lesson_34/HW_L34/scripts/task_2.js
@@ -7,20 +7,7 @@ function shuffle(tempList) {
     }
 }
 
-function startApp() {
-    const images = [
-        {src: './assets/1.jpg', alt: 'photo of leopard 1'}, {src: './assets/2.jpg', alt: 'photo of leopard 2'},
-        {src: './assets/3.jpg', alt: 'photo of leopard 3'}, {src: './assets/4.jpg', alt: 'photo of leopard 4'},
-        {src: './assets/5.jpg', alt: 'photo of leopard 5'}, {src: './assets/6.jpg', alt: 'photo of leopard 6'},
-        {src: './assets/7.jpg', alt: 'photo of leopard 7'}, {src: './assets/8.jpg', alt: 'photo of leopard 8'},
-        {src: './assets/9.jpg', alt: 'photo of leopard 9'}, {src: './assets/10.jpg', alt: 'photo of leopard 10'},
-        {src: './assets/11.jpg', alt: 'photo of leopard 11'}, {src: './assets/12.jpg', alt: 'photo of leopard 12'},
-        {src: './assets/13.jpg', alt: 'photo of leopard 13'}, {src: './assets/14.jpg', alt: 'photo of leopard 14'},
-        {src: './assets/15.jpg', alt: 'photo of leopard 15'}, {src: './assets/16.jpg', alt: 'photo of leopard 16'}
-    ]
-
-    shuffle(images);
-
+function createSection(images) {
     const section = document.createElement('section');
     section.className = 'gallery';
     const wrapper = document.createElement('div');
@@ -39,6 +26,27 @@ function startApp() {
     })
     const main = document.querySelector('main');
     main.append(section);
+
+    wrapper.addEventListener('click', () => {
+        main.innerHTML = '';
+        shuffle(images);
+        createSection(images);
+    });
+}
+
+function startApp() {
+    const images = [
+        {src: './assets/1.jpg', alt: 'photo of leopard 1'}, {src: './assets/2.jpg', alt: 'photo of leopard 2'},
+        {src: './assets/3.jpg', alt: 'photo of leopard 3'}, {src: './assets/4.jpg', alt: 'photo of leopard 4'},
+        {src: './assets/5.jpg', alt: 'photo of leopard 5'}, {src: './assets/6.jpg', alt: 'photo of leopard 6'},
+        {src: './assets/7.jpg', alt: 'photo of leopard 7'}, {src: './assets/8.jpg', alt: 'photo of leopard 8'},
+        {src: './assets/9.jpg', alt: 'photo of leopard 9'}, {src: './assets/10.jpg', alt: 'photo of leopard 10'},
+        {src: './assets/11.jpg', alt: 'photo of leopard 11'}, {src: './assets/12.jpg', alt: 'photo of leopard 12'},
+        {src: './assets/13.jpg', alt: 'photo of leopard 13'}, {src: './assets/14.jpg', alt: 'photo of leopard 14'},
+        {src: './assets/15.jpg', alt: 'photo of leopard 15'}, {src: './assets/16.jpg', alt: 'photo of leopard 16'}
+    ]
+
+    createSection(images);
 }
 
 startApp();
