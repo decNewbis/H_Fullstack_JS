5bed8f97b3072e9cb2ef9c3eb99a1dbe8846e4d7
decodload <decodload@gmail.com>
2024-04-19 21:32:40 +0300

    [feature] hw_L37: create func 'editCurrentTodo'

--- lesson_37/HW_L37/scripts/todoList.js
+++ lesson_37/HW_L37/scripts/todoList.js
@@ -1,12 +1,24 @@
 // task todoList
 
 function saveTodo() {
-    localStorage.setItem('listOfTodo', JSON.stringify(pageObj.listArray));
+    localStorage.setItem('listOfTodo', JSON.stringify(pageObj.listArrayOfContent));
+}
+
+
+function deleteCurrentTodo() {
+    const textContent = this.listItem.querySelector('span.todo__content').textContent.trim();
+    const indexOfCurrentContent = pageObj.listArrayOfContent.indexOf(textContent);
+    console.log(indexOfCurrentContent);
+    if (indexOfCurrentContent !== -1) {
+        pageObj.listArrayOfContent.splice(indexOfCurrentContent, 1);
+    }
+    this.listItem.remove();
+    saveTodo();
 }
 
 function addTodoItem() {
     if (!pageObj.inputField.value) { return }
-    const value = pageObj.inputField.value;
+    const value = pageObj.inputField.value.trim();
     const listItemContentTemplate = `
     <span class="todo__content">
         ${value}
@@ -20,19 +32,58 @@ function addTodoItem() {
     const listItem = document.createElement('li');
     listItem.className = 'todo__item';
     listItem.innerHTML = listItemContentTemplate;
+    const deleteBtn = listItem.querySelector('button.todo__content-deleteBtn');
+    deleteBtn.addEventListener('click', deleteCurrentTodo.bind({listItem: listItem}));
     pageObj.todoList.prepend(listItem);
-    pageObj.listArray.push(value);
+    pageObj.listArrayOfContent.push(value);
     pageObj.inputField.value = '';
     saveTodo();
 }
 
+function editCurrentTodo() {
+    const content = this.listItem.querySelector('li > span');
+    const textContent = content.textContent.trim();
+    const editArea = document.createElement('input');
+    const indexOfCurrentContent = pageObj.listArrayOfContent.indexOf(textContent);
+    editArea.setAttribute('type', 'text');
+    editArea.className = content.className;
+    editArea.classList.toggle('borders-extra-param');
+    editArea.value = textContent;
+    content.remove();
+    this.listItem.prepend(editArea);
+    editArea.focus();
+    this.editBtn.style.display = 'none';
+    this.deleteBtn.style.display = 'none';
+    this.applyBtn.style.display = 'block';
+    this.applyBtn.addEventListener('click', () => {
+        content.textContent = editArea.value.trim();
+        editArea.remove();
+        this.listItem.prepend(content);
+        if (indexOfCurrentContent !== -1) {
+            pageObj.listArrayOfContent.splice(indexOfCurrentContent, 1, content.textContent);
+            saveTodo();
+        }
+        this.editBtn.style.display = 'block';
+        this.deleteBtn.style.display = 'block';
+        this.applyBtn.style.display = 'none';
+    }, {once: true});
+    this.listItem.addEventListener('focusout', (event) => {
+        editArea.remove();
+        console.log(this.listItem);
+        this.listItem.prepend(content);
+        this.editBtn.style.display = 'block';
+        this.deleteBtn.style.display = 'block';
+        this.applyBtn.style.display = 'none';
+    }, {once: true});
+}
+
 function loadTodo() {
-    pageObj.listArray = JSON.parse(localStorage.getItem('listOfTodo')) || [];
-    if (pageObj.listArray) {
-        pageObj.listArray.forEach((element) => {
+    pageObj.listArrayOfContent = JSON.parse(localStorage.getItem('listOfTodo')) || [];
+    if (pageObj.listArrayOfContent) {
+        pageObj.listArrayOfContent.forEach((value) => {
             const listItemContentTemplate = `
                 <span class="todo__content">
-                    ${element}
+                    ${value}
                 </span>
                 <div class="todo__content-btnBar">
                     <button class="todo__content-editBtn">✏️</button>
@@ -43,6 +94,18 @@ function loadTodo() {
             const listItem = document.createElement('li');
             listItem.className = 'todo__item';
             listItem.innerHTML = listItemContentTemplate;
+            const editBtn = listItem.querySelector('button.todo__content-editBtn');
+            const applyBtn = listItem.querySelector('button.todo__content-applyBtn');
+            const deleteBtn = listItem.querySelector('button.todo__content-deleteBtn');
+            deleteBtn.addEventListener('click', deleteCurrentTodo.bind({listItem: listItem}));
+            editBtn.addEventListener('click', editCurrentTodo.bind(
+                    {
+                        listItem: listItem,
+                        editBtn: editBtn,
+                        deleteBtn: deleteBtn,
+                        applyBtn: applyBtn
+                    }
+                ));
             pageObj.todoList.prepend(listItem);
         });
     }
@@ -57,7 +120,7 @@ const pageObj = {
     inputField: document.getElementById('inputField'),
     addTodoBtn: document.getElementById('addTodo'),
     todoList: document.getElementById('todoList'),
-    listArray: [],
+    listArrayOfContent: [],
 }
 
 startApp();
