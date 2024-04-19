ea1d58576c8431ea66772254aba22b24ba084e57
decodload <decodload@gmail.com>
2024-04-19 04:41:54 +0300

    [feature] hw_L37: create func 'addTodoItem'

--- lesson_37/HW_L37/scripts/todoList.js
+++ lesson_37/HW_L37/scripts/todoList.js
@@ -1,1 +1,32 @@
 // task todoList
+
+function addTodoItem() {
+    if (!pageObj.inputField.value) { return };
+    const listItemContentTemplate = `
+    <span class="todo__content">
+        ${pageObj.inputField.value}
+    </span>
+    <div class="todo__content-btnBar">
+        <button class="todo__content-editBtn">✏️</button>
+        <button class="todo__content-applyBtn">✔️</button>
+        <button class="todo__content-deleteBtn">🗑️</button>
+    </div>
+    `;
+    const listItem = document.createElement('li');
+    listItem.className = 'todo__item';
+    listItem.innerHTML = listItemContentTemplate;
+    pageObj.todoList.prepend(listItem);
+    pageObj.inputField.value = '';
+}
+
+function startApp() {
+    pageObj.addTodoBtn.addEventListener('click', addTodoItem);
+}
+
+const pageObj = {
+    inputField: document.getElementById('inputField'),
+    addTodoBtn: document.getElementById('addTodo'),
+    todoList: document.getElementById('todoList'),
+}
+
+startApp();
