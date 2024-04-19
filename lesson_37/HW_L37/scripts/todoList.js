7e4f8456610d8dd83b640e688d6ad1ff0abcbbd7
decodload <decodload@gmail.com>
2024-04-19 05:06:18 +0300

    [feature] hw_L37: create func 'saveTodo', added list of todo as array

--- lesson_37/HW_L37/scripts/todoList.js
+++ lesson_37/HW_L37/scripts/todoList.js
@@ -1,10 +1,15 @@
 // task todoList
 
+function saveTodo() {
+    localStorage.setItem('listOfTodo', JSON.stringify(pageObj.listArray));
+}
+
 function addTodoItem() {
-    if (!pageObj.inputField.value) { return };
+    if (!pageObj.inputField.value) { return }
+    const value = pageObj.inputField.value;
     const listItemContentTemplate = `
     <span class="todo__content">
-        ${pageObj.inputField.value}
+        ${value}
     </span>
     <div class="todo__content-btnBar">
         <button class="todo__content-editBtn">✏️</button>
@@ -16,17 +21,23 @@ function addTodoItem() {
     listItem.className = 'todo__item';
     listItem.innerHTML = listItemContentTemplate;
     pageObj.todoList.prepend(listItem);
+    pageObj.listArray.push(value);
     pageObj.inputField.value = '';
+    saveTodo();
 }
 
 function startApp() {
     pageObj.addTodoBtn.addEventListener('click', addTodoItem);
+    const a = JSON.stringify({a: 'asdsdad', b: 'lknkjdfhg'});
+    console.log(a);
+    console.log(JSON.parse(a));
 }
 
 const pageObj = {
     inputField: document.getElementById('inputField'),
     addTodoBtn: document.getElementById('addTodo'),
     todoList: document.getElementById('todoList'),
+    listArray: [],
 }
 
 startApp();
