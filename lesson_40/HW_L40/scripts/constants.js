1d144fb0bbb2dab01e4b585b4642b1d5129d42fe
decodload <decodload@gmail.com>
2024-05-19 01:14:32 +0300

    [feature] hw_L40: added 'ACCESS_KEY' and BACKGROUND_API_DOMAIN

--- lesson_40/HW_L40/scripts/constants.js
+++ lesson_40/HW_L40/scripts/constants.js
@@ -1,2 +1,6 @@
-const API_KEY = "7bb094a799bb636f0901353c7289624d";
-export const WEATHER_API_DOMAIN = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`
+const API_KEY = "dffb2a6aff2b4d8db0ebb67b53745e61";
+const ACCESS_KEY = "rGUCL2eTNSwGCuS1dg59ChUFa8U0J4u6KdeIjGpU7S4";
+const WEATHER_API_DOMAIN = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
+const BACKGROUND_API_DOMAIN = `https://api.unsplash.com/search/photos?page=1&client_id=${ACCESS_KEY}`;
+
+export {WEATHER_API_DOMAIN, BACKGROUND_API_DOMAIN};
