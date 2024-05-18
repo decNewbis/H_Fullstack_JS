57ad08434542b8314b6b19436787ddabd43f2001
decodload <decodload@gmail.com>
2024-05-19 01:15:20 +0300

    [feature] hw_L40: added functions 'getBackgroundImages' and 'logError'

--- lesson_40/HW_L40/scripts/index.js
+++ lesson_40/HW_L40/scripts/index.js
@@ -1,15 +1,24 @@
-import { WEATHER_API_DOMAIN } from './constants.js';
-import { fillDetails } from './showElements.js';
+import { WEATHER_API_DOMAIN, BACKGROUND_API_DOMAIN } from './constants.js';
+import { fillDetails, setBackground } from './showElements.js';
+
+function logError(error) {
+    console.log(error);
+    console.error(error.message);
+    alert('something went wrong, please return to us in few minutes');
+}
 
 function getWeatherDetails(weatherUrl) {
     fetch(weatherUrl)
         .then((data) => data.json())
         .then(fillDetails)
-        .catch((error) => {
-            console.log(error)
-            console.error(error.message);
-            alert('something went wrong, please return to us in few minutes')
-        })
+        .catch(logError);
+}
+
+function getBackgroundImages(backgroundUrl) {
+    fetch(backgroundUrl)
+        .then((data) => data.json())
+        .then(setBackground)
+        .catch(logError);
 }
 
 function handleSubmit(event) {
@@ -17,6 +26,7 @@ function handleSubmit(event) {
     const city = document.getElementById('city').value.trim();
     if (city?.length >= 1) {
         getWeatherDetails(`${WEATHER_API_DOMAIN}&q=${city}`);
+        getBackgroundImages(`${BACKGROUND_API_DOMAIN}&query=${city}`);
     }
 }
 
