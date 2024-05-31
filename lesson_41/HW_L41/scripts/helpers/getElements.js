e086c493458eea1e812ee223cf0e9b018479389b
decodload <decodload@gmail.com>
2024-05-31 18:53:37 +0300

    [feature] hw_L41: added func 'getSearchButton'

--- lesson_41/HW_L41/scripts/helpers/getElements.js
+++ lesson_41/HW_L41/scripts/helpers/getElements.js
@@ -1,5 +1,8 @@
 export function getSearchElement() {
     return document.getElementById("search-box");
+
+}export function getSearchButton() {
+    return document.getElementById("search-button");
 }
 
 export function getPostersContainer() {
