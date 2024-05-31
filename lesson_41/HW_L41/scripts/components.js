c36d282be663d72c3a51c674ac5d3ef301d25449
decodload <decodload@gmail.com>
2024-05-31 18:54:47 +0300

    [feature] hw_L41: added input[radio] into filters block

--- lesson_41/HW_L41/scripts/components.js
+++ lesson_41/HW_L41/scripts/components.js
@@ -23,42 +23,39 @@ const movieNotFound = "<li class='preview-item'>Movie wasn't found</li>"
 
 const renderMainComponent = () => {
     document.getElementById('app').innerHTML = `
-<div class="container">
-    <aside class="filters">
-        <h2>Filters</h2>
-        <div>
-            <input type="checkbox" id="movies" name="type" value="movies">
-            <label for="movies">Movies</label>
-        </div>
-        <div>
-            <input type="checkbox" id="series" name="type" value="series">
-            <label for="series">Series</label>
-            <!-- Additional fields for series -->
-            <div class="series-options" style="display: none;">
-                <input type="number" id="season" placeholder="Season" min="1">
-                <input type="number" id="episode" placeholder="Episode" min="1">
-            </div>
-        </div>
-        <div>
-            <input type="checkbox" id="cartoons" name="type" value="cartoons">
-            <label for="cartoons">Cartoons</label>
-        </div>
-        <div>
-            <label for="year">Release Year:</label>
-            <input type="number" id="year" placeholder="2023" min="1900" max="2023">
-        </div>
-    </aside>
-    <main class="content">
-        <div class="search-container">
-            <div class="search-wrapper">
-                <input type="search" class="search" id="search-box" placeholder="Enter a title...">
-                <ul id="preview-list" class="preview-list hidden"></ul>
-            </div>
-            <button id="search-button">Search</button>
-        </div>
-        <div id="posters" class="results-container"></div>
-    </main>
-</div>    
+        <div class="container">
+            <aside class="filters">
+                <h2>Filters</h2>
+                <fieldset>
+                    <div>
+                        <input type="radio" id="both" name="type" value="both" checked>
+                        <label for="both">Movie and series</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="movies" name="type" value="movies">
+                        <label for="movies">Movies</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="series" name="type" value="series">
+                        <label for="series">Series</label>
+                    </div>
+                </fieldset>
+                <div>
+                    <label for="year">Release Year:</label>
+                    <input type="number" id="year" placeholder="2023" min="1900" max="2023">
+                </div>
+            </aside>
+            <main class="content">
+                <div class="search-container">
+                    <div class="search-wrapper">
+                        <input type="search" class="search" id="search-box" placeholder="Enter a title...">
+                        <ul id="preview-list" class="preview-list hidden"></ul>
+                    </div>
+                    <button id="search-button">Search</button>
+                </div>
+                <div id="posters" class="results-container"></div>
+            </main>
+        </div>    
     `
 }
 
