d9e13758a8394dd65d46fa2b73b97020dd546ad8
decodload <decodload@gmail.com>
2024-06-11 14:08:19 +0300

    [refactor] hw_L46: component 'Header' - added 'NotificationLine'

--- lesson_46/HW_L46/demy-app/src/components/Header/Header.jsx
+++ lesson_46/HW_L46/demy-app/src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import "./_header.scss";
 
 import {Navbar} from "../Navbar";
+import {NotificationLine} from "../NotificationLine";
 
 export function Header() {
   return (
     <header className="header">
+      <NotificationLine />
       <div className="header__wrapper">
         <Navbar />
       </div>
