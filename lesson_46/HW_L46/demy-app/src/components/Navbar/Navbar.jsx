ba000a1ab003c2c176779e39e6a420955137f901
decodload <decodload@gmail.com>
2024-06-11 04:53:02 +0300

    [refactor] hw_L46: component 'Navbr' - deleted box-shadow

---
+++ lesson_46/HW_L46/demy-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import "./_navbar.scss";
+import logo from "../../assets/svg/logo.svg";
+import {Search} from "../Search";
+import {Buttonbar} from "../Buttonbar";
+
+export function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="navbar__list">
+        <li className="navbar__item">
+          <a href="/">
+            <img src={logo} alt="logo"/>
+          </a>
+        </li>
+        <li className="navbar__item">
+          <a href="#">Categories</a>
+        </li>
+        <li className="navbar__item">
+          <Search />
+        </li>
+        <li className="navbar__item">
+          <a href="#">Demy Business</a>
+        </li>
+        <li className="navbar__item">
+          <a href="#">Teach on Demy</a>
+        </li>
+        <li className="navbar__item">
+          <Buttonbar />
+        </li>
+      </ul>
+    </nav>
+  );
+}
