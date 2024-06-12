9eb0de13dfeae772e9a925d6817c30bb122d2a32
decodload <decodload@gmail.com>
2024-06-12 17:49:18 +0300

    [refactor] hw_L47: 'NotificationLine' - created logic of button 'close' with react way, included localStorage

--- lesson_46/HW_L46/demy-app/src/components/Header/Header.jsx
+++ lesson_46/HW_L46/demy-app/src/components/Header/Header.jsx
@@ -3,10 +3,26 @@ import "./_header.scss";
 import {Navbar} from "../Navbar";
 import {NotificationLine} from "../NotificationLine";
 
+import { useState } from "react";
+
 export function Header() {
+  let currentNotificationLineVisible = JSON.parse(localStorage.getItem("currentNotificationLineVisible"));
+  if (currentNotificationLineVisible === null) {
+    currentNotificationLineVisible = true;
+  }
+  const [isNotificationLineVisible, setNotificationVisible] = useState(currentNotificationLineVisible);
+  const toggleNotificationLineVisible = () => {
+    const currentState = !isNotificationLineVisible;
+    setNotificationVisible(currentState);
+    localStorage.setItem("currentNotificationLineVisible", JSON.stringify(currentState));
+  }
   return (
     <header className="header">
-      <NotificationLine />
+      {
+        isNotificationLineVisible
+          ? <NotificationLine onClick={toggleNotificationLineVisible}/>
+          : null
+      }
       <div className="header__wrapper">
         <Navbar />
       </div>
--- lesson_46/HW_L46/demy-app/src/components/NotificationLine/NotificationLine.jsx
+++ lesson_46/HW_L46/demy-app/src/components/NotificationLine/NotificationLine.jsx
@@ -1,20 +1,12 @@
 import "./_notification-line.scss";
 
-import React, {useRef} from "react";
-
-export function NotificationLine() {
-  const notificationRef = useRef(null);
-  const handleCloseClick = () => {
-    if (notificationRef.current) {
-      notificationRef.current?.classList.toggle('hidden');
-    }
-  };
+export function NotificationLine({onClick}) {
 
   return (
-    <div className="notification" ref={notificationRef}>
+    <div className="notification">
       <div className="notification__wrapper">
           <div className="notification__line">
-            <span className="notification__close" onClick={handleCloseClick}>x</span>
+            <span className="notification__close" onClick={onClick}>x</span>
             <span className="notification__message">
               New to Demy? Learning leds to opportunity
             </span>
