911701ef819779253802a1cd6ba54b90ad06ee4e
decodload <decodload@gmail.com>
2024-06-14 20:01:52 +0300

    [refactor] hw_L47: component 'Main' - added section

--- lesson_46/HW_L46/demy-app/src/components/Main/Main.jsx
+++ lesson_46/HW_L46/demy-app/src/components/Main/Main.jsx
@@ -5,17 +5,19 @@ import {Reviews} from "../Rviews";
 export function Main() {
   return (
     <main className="main">
-      <div className="main__wrapper main__background-image">
-        <div className="main__content">
-          <h1 className="main__title">
-            Jump into learning for less
-          </h1>
-          <p className="main__paragraph">
-            If you are new to Demy, we've got good news: For a limited time, courses start at just €14.99 for new
-            learners. Shop now!
-          </p>
+      <section>
+        <div className="main__wrapper main__background-image">
+          <div className="main__content">
+            <h1 className="main__title">
+              Jump into learning for less
+            </h1>
+            <p className="main__paragraph">
+              If you are new to Demy, we've got good news: For a limited time, courses start at just €14.99 for new
+              learners. Shop now!
+            </p>
+          </div>
         </div>
-      </div>
+      </section>
       <Partners />
       <Reviews />
     </main>
