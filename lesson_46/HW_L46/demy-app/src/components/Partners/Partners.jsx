38b4a6b09d5502d588d551046330b62cf2db60e7
decodload <decodload@gmail.com>
2024-06-14 10:50:48 +0300

    [feature] hw_L47: component 'Partners' - added 'companiesLogoDb' and exported

--- lesson_46/HW_L46/demy-app/src/components/Partners/Partners.jsx
+++ lesson_46/HW_L46/demy-app/src/components/Partners/Partners.jsx
@@ -3,16 +3,7 @@ import "./_media-partners.scss";
 
 import {images} from "./importImages";
 
-const companiesLogo = [
-  {srcLogo: "vw.png", name: "volkswagen"},
-  {srcLogo: "samsung.png", name: "samsung"},
-  {srcLogo: "cisco.png", name: "cisco"},
-  {srcLogo: "at_t.png", name: "at&t"},
-  {srcLogo: "p_g.png", name: "p&g"},
-  {srcLogo: "hp.png", name: "hp"},
-  {srcLogo: "citi.png", name: "citi"},
-  {srcLogo: "ericsson.png", name: "ericsson"},
-]
+import {companiesLogoDb} from "./companiesLogoDb";
 
 export function Partners() {
 
@@ -24,7 +15,7 @@ export function Partners() {
         </h2>
         <ul className="partners__list">
           {
-            companiesLogo.map(({srcLogo, name}) => {
+            companiesLogoDb.map(({srcLogo, name}) => {
               return (
                 <li key={name}>
                   <img className="partners__logo" src={images[srcLogo]} alt={name}/>
