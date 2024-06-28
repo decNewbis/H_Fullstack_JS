0c8dd0c97d9b2ec4d03580d2cb55ce47f6f01ec6
decodload <decodload@gmail.com>
2024-06-28 22:00:45 +0300

    [feature] hw_L51: created new component 'DishDetails'

---
+++ lesson_50-51/HW_L50-51/src/pages/DishDetails/DishDetails.js
@@ -0,0 +1,57 @@
+import {useParams} from "react-router-dom";
+import {Grid, Typography, List, ListItem, ListItemText} from "@mui/material";
+import {useData} from "../../hooks";
+import {Main} from "../../styledComponents/Main";
+import {Paragraph} from "../../styledComponents/Paragraph";
+import {Image} from "../../styledComponents/Image";
+import {API} from "../../constants";
+
+
+export const DishDetails = () => {
+  let {dishId} = useParams();
+  const data = useData(`${API.dishDetails}${dishId}`)
+  console.log('data', data)
+
+  if(!data?.meals) {
+    return null;
+  }
+  const dataDetails = data.meals[0];
+
+  const ingredients = [];
+  for (let i=1; i <= 20; i++) {
+    const ingredient = dataDetails[`strIngredient${i}`];
+    const measure = dataDetails[`strMeasure${i}`];
+    if (ingredient) {
+      ingredients.push({ id: i, ingredient, measure });
+    }
+  }
+  console.log('ingredients', ingredients);
+
+  return (
+    <Main>
+      <Grid container spacing={2} sx={{minWidth: 720, maxWidth: 855}}>
+        <Grid xs={8}>
+          <Image src={dataDetails.strMealThumb} alt={dataDetails.strMeal} maxWidth="450px" />
+        </Grid>
+        <Grid xs={4}>
+          <Typography sx={{ mt: 2, mb: 1 }} variant="h6" component="div">
+            Ingredients:
+          </Typography>
+          <List dense={false}>
+          {ingredients.map(({ id, ingredient, measure }) => (
+            <ListItem sx={{pt:0, pb:0}}>
+              <ListItemText
+                primary={`${id}. ${ingredient}: ${measure}`}
+              />
+            </ListItem>
+          ))}
+          </List>
+        </Grid>
+        <Grid xs={12}>
+          <h1>Instructions</h1>
+          <Paragraph>{dataDetails.strInstructions}</Paragraph>
+        </Grid>
+      </Grid>
+    </Main>
+  );
+}
