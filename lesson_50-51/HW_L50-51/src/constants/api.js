cacf1c57ea43adc37267cd12de6bd6b9ea413589
decodload <decodload@gmail.com>
2024-06-28 23:08:03 +0300

    [feature] hw_L51: constant 'API' - added 'ingredientThumbnail' link

--- lesson_50-51/HW_L50-51/src/constants/api.js
+++ lesson_50-51/HW_L50-51/src/constants/api.js
@@ -3,5 +3,6 @@ const domain = "https://www.themealdb.com/api/json/v1/1"
 export const API = {
   category: `${domain}/filter.php?c=`,
   categories: `${domain}/list.php?c=list`,
-  dishDetails: `${domain}/lookup.php?i=`
+  dishDetails: `${domain}/lookup.php?i=`,
+  ingredientThumbnail: 'https://www.themealdb.com/images/ingredients/'
 }
