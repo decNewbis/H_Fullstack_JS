6a175b36d46477e86382cdf14dd9436385b71a55
decodload <decodload@gmail.com>
2024-07-05 04:34:35 +0300

    [feature] hw_L52: created mockCourses

---
+++ lesson_46/HW_L46/demy-app/src/pages/CoursesPage/mock.js
@@ -0,0 +1,58 @@
+import {v4 as uuidv4} from "uuid";
+
+export const mockCourses = [
+  {
+    id: uuidv4(),
+    title: '100 Days of Code: The Complete Python Pro Bootcamp',
+    author: 'CodeUA',
+    link: 'https://www.youtube.com/shorts/aiWjUuLWrAo?si=nIZU1o5606KMC5nZ',
+    description: 'Master Python by building 100 projects in 100 days. Learn data science, automation, ' +
+      'build websites, games and apps!'
+  },
+  {
+    id: uuidv4(),
+    title: '[NEW] Ultimate AWS Certified Cloud Practitioner CLF-C02',
+    author: 'Anton Robin S.',
+    link: 'https://www.youtube.com/shorts/aiWjUuLWrAo?si=nIZU1o5606KMC5nZ',
+    description: 'Full Practice Exam included + explanations | Learn Cloud Computing | Pass the AWS Cloud ' +
+      'Practitioner CLF-C02 exam!'
+  },
+  {
+    id: uuidv4(),
+    title: 'Automate the Boring Stuff with Python Programming',
+    author: 'Anthony N.',
+    link: 'https://www.youtube.com/shorts/aiWjUuLWrAo?si=nIZU1o5606KMC5nZ',
+    description: 'A practical programming course for office workers, academics, and administrators who want to' +
+      ' improve their productivity.'
+  },
+  {
+    id: uuidv4(),
+    title: 'Scratch Game Programming',
+    author: 'Antony H.',
+    link: 'https://www.youtube.com/shorts/aiWjUuLWrAo?si=nIZU1o5606KMC5nZ',
+    description: 'A fun guide to programming for parents & teachers who want to help kids learn to code.'
+  },
+  {
+    id: uuidv4(),
+    title: 'React - The Complete Guide 2024 (incl. Next.js, Redux)',
+    author: 'Obada Q.',
+    link: 'https://www.youtube.com/shorts/aiWjUuLWrAo?si=nIZU1o5606KMC5nZ',
+    description: 'Dive in and learn React.js from scratch! Learn React, Hooks, Redux, React Router, ' +
+      'Next.js, Best Practices and way more!'
+  },
+  {
+    id: uuidv4(),
+    title: 'Typescript & React JS Course with React & Typescript Project',
+    author: 'Marcela Fernández P.',
+    link: 'https://www.youtube.com/shorts/aiWjUuLWrAo?si=nIZU1o5606KMC5nZ',
+    description: 'Typescript and ReactJS course helps you understand React JS &amp; Type script in detail with real' +
+      ' Typescript React Projects'
+  },
+  {
+    id: uuidv4(),
+    title: 'React JS- Complete Guide for Frontend Web Development',
+    author: 'Qazi M.',
+    link: 'https://www.youtube.com/shorts/aiWjUuLWrAo?si=nIZU1o5606KMC5nZ',
+    description: 'Become an expert React JS Developer. Learn HTML, CSS, JavaScript, ES6, React JS and jQuery.'
+  }
+];
