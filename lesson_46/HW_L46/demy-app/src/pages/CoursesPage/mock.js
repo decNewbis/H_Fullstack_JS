27e9beca6c584d022e574efb2c4e76847900afd5
decodload <decodload@gmail.com>
2024-07-05 04:59:45 +0300

    [refactor] hw_L52: added fixed ids to mock courses

--- lesson_46/HW_L46/demy-app/src/pages/CoursesPage/mock.js
+++ lesson_46/HW_L46/demy-app/src/pages/CoursesPage/mock.js
@@ -1,8 +1,6 @@
-import {v4 as uuidv4} from "uuid";
-
 export const mockCourses = [
   {
-    id: uuidv4(),
+    id: "ee4f5ea6-044a-4e9b-8b18-d48e95812336",
     title: '100 Days of Code: The Complete Python Pro Bootcamp',
     author: 'CodeUA',
     link: 'https://www.youtube.com/shorts/aiWjUuLWrAo?si=nIZU1o5606KMC5nZ',
@@ -10,7 +8,7 @@ export const mockCourses = [
       'build websites, games and apps!'
   },
   {
-    id: uuidv4(),
+    id: "ba9ac3d6-4c11-4ef0-97e8-852681446a86",
     title: '[NEW] Ultimate AWS Certified Cloud Practitioner CLF-C02',
     author: 'Anton Robin S.',
     link: 'https://www.youtube.com/shorts/aiWjUuLWrAo?si=nIZU1o5606KMC5nZ',
@@ -18,7 +16,7 @@ export const mockCourses = [
       'Practitioner CLF-C02 exam!'
   },
   {
-    id: uuidv4(),
+    id: "92e4482a-66ef-4b49-b0a9-fb7c365a30d5",
     title: 'Automate the Boring Stuff with Python Programming',
     author: 'Anthony N.',
     link: 'https://www.youtube.com/shorts/aiWjUuLWrAo?si=nIZU1o5606KMC5nZ',
@@ -26,14 +24,14 @@ export const mockCourses = [
       ' improve their productivity.'
   },
   {
-    id: uuidv4(),
+    id: "88004269-097c-4cb1-9738-b639d13e26d2",
     title: 'Scratch Game Programming',
     author: 'Antony H.',
     link: 'https://www.youtube.com/shorts/aiWjUuLWrAo?si=nIZU1o5606KMC5nZ',
     description: 'A fun guide to programming for parents & teachers who want to help kids learn to code.'
   },
   {
-    id: uuidv4(),
+    id: "5e9246fd-e2ed-4199-9c18-bd9c0dbac98b",
     title: 'React - The Complete Guide 2024 (incl. Next.js, Redux)',
     author: 'Obada Q.',
     link: 'https://www.youtube.com/shorts/aiWjUuLWrAo?si=nIZU1o5606KMC5nZ',
@@ -41,7 +39,7 @@ export const mockCourses = [
       'Next.js, Best Practices and way more!'
   },
   {
-    id: uuidv4(),
+    id: "93a65ea3-296a-45ef-8107-087564a1871a",
     title: 'Typescript & React JS Course with React & Typescript Project',
     author: 'Marcela Fernández P.',
     link: 'https://www.youtube.com/shorts/aiWjUuLWrAo?si=nIZU1o5606KMC5nZ',
@@ -49,7 +47,7 @@ export const mockCourses = [
       ' Typescript React Projects'
   },
   {
-    id: uuidv4(),
+    id: "866c61f9-d6ce-4e6f-a9cc-25845e3f64bf",
     title: 'React JS- Complete Guide for Frontend Web Development',
     author: 'Qazi M.',
     link: 'https://www.youtube.com/shorts/aiWjUuLWrAo?si=nIZU1o5606KMC5nZ',
