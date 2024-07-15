1da5d148997512e48dd24bf95c01ff0dc649587f
decodload <decodload@gmail.com>
2024-07-16 00:23:19 +0300

    [refactor] hw_L56: test-component 'Tabs' - added data-testid attributes

--- lesson_46/HW_L46/demy-app/src/hometask56/components/Tabs/Tabs.jsx
+++ lesson_46/HW_L46/demy-app/src/hometask56/components/Tabs/Tabs.jsx
@@ -6,14 +6,14 @@ export const Tabs = () => {
   return (
     <div>
       <div>
-        <button onClick={() => setActiveTab('Tab1')}>Tab 1</button>
-        <button onClick={() => setActiveTab('Tab2')}>Tab 2</button>
-        <button onClick={() => setActiveTab('Tab3')}>Tab 3</button>
+        <button data-testid="tab1Button" onClick={() => setActiveTab('Tab1')}>Tab 1</button>
+        <button data-testid="tab2Button" onClick={() => setActiveTab('Tab2')}>Tab 2</button>
+        <button data-testid="tab3Button" onClick={() => setActiveTab('Tab3')}>Tab 3</button>
       </div>
       <div>
-        {activeTab === 'Tab1' && <p>Content of Tab 1</p>}
-        {activeTab === 'Tab2' && <p>Content of Tab 2</p>}
-        {activeTab === 'Tab3' && <p>Content of Tab 3</p>}
+        {activeTab === 'Tab1' && <p data-testid="tab1Content">Content of Tab 1</p>}
+        {activeTab === 'Tab2' && <p data-testid="tab2Content">Content of Tab 2</p>}
+        {activeTab === 'Tab3' && <p data-testid="tab3Content">Content of Tab 3</p>}
       </div>
     </div>
   );
