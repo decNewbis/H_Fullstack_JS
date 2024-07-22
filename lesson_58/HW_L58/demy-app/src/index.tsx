aa958de8fe952913eb0c09948cc53eb64c9cdc7e
decodload <decodload@gmail.com>
2024-07-22 12:28:56 +0300

    [refactor] hw_L58: [TS] main component 'index.js' converted to 'index.tsx'

--- lesson_58/HW_L58/demy-app/src/index.tsx
+++ lesson_58/HW_L58/demy-app/src/index.tsx
@@ -5,7 +5,7 @@ import {Provider} from 'react-redux';
 import './index.css';
 import {App} from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
