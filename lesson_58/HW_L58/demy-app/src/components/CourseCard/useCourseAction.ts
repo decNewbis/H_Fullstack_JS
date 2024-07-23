145094aa4b824cc22d0a729907d25feb79eef020
decodload <decodload@gmail.com>
2024-07-23 19:02:32 +0300

    [refactor] hw_L58: [TS] component 'useCourseAction.js' converted to 'CourseCard.ts'

--- lesson_58/HW_L58/demy-app/src/components/CourseCard/useCourseAction.ts
+++ lesson_58/HW_L58/demy-app/src/components/CourseCard/useCourseAction.ts
@@ -1,20 +1,48 @@
 import {useState} from "react";
+import {RootState} from "../../store"
 import {useDispatch, useSelector} from "react-redux";
 import {deleteCourse, saveCourse} from "../../store/slices/courseSlice";
 import {addToFavorite, deleteFromFavorite} from "../../store/slices/profileSlice";
 
-export const useCourseAction = (courseContext) => {
+interface CourseContextProps {
+  id: string,
+  title: string,
+  author: string,
+  link: string,
+  description: string,
+  editTitle:string,
+  setTitle:(title: string) => void,
+  editAuthor:string,
+  setAuthor:(author: string) => void,
+  editLink:string,
+  setLink:(link: string) => void,
+  editDescription:string,
+  setDescription:(description: string) => void,
+}
+
+interface UseCourseActionResult {
+  isEdit: boolean,
+  isFavorite: boolean,
+  editHandler: () => void,
+  saveHandler: () => void,
+  cancelHandler: () => void,
+  deleteHandler: () => void,
+  addToFavoriteHandler: () => void,
+  deleteFromFavoriteHandler: () => void,
+}
+
+export const useCourseAction = (courseContext: CourseContextProps) => {
   const {
     id, title, author, link, description,
     editTitle, setTitle, editAuthor, setAuthor,
     editLink, setLink, editDescription, setDescription
   } = courseContext;
 
-  const [isEdit, setIsEdit] = useState(false);
+  const [isEdit, setIsEdit] = useState<boolean>(false);
   const initialIsFavoriteState = Boolean(
-    useSelector(state => state.profile.favoriteList.find(item => item.id === id))
+    useSelector((state: RootState) => state.profile.favoriteList.find(item => item.id === id))
   );
-  const [isFavorite, setIsFavorite] = useState(initialIsFavoriteState);
+  const [isFavorite, setIsFavorite] = useState<boolean>(initialIsFavoriteState);
   const dispatch = useDispatch();
 
   const editHandler = () => {
