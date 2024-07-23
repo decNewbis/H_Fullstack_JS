149b9b16604107b455bfc363b6e52c6c5f6c5e4d
decodload <decodload@gmail.com>
2024-07-23 19:11:29 +0300

    [refactor] hw_L58: [TS] component 'profileSlice.js' converted to 'profileSlice.ts'

--- lesson_58/HW_L58/demy-app/src/store/slices/profileSlice.ts
+++ lesson_58/HW_L58/demy-app/src/store/slices/profileSlice.ts
@@ -1,7 +1,22 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {v4 as uuidv4} from 'uuid';
 
-const initialState = {
+interface FavoriteItem {
+  id: string;
+  title: string;
+  author: string;
+  link: string;
+  description: string;
+}
+
+interface ProfileState {
+  userId: string;
+  firstName: string;
+  lastName: string;
+  favoriteList: FavoriteItem[];
+}
+
+const initialState: ProfileState = {
   userId: uuidv4(),
   firstName: "Admin",
   lastName: "Admin",
@@ -12,13 +27,13 @@ export const profileSlice = createSlice({
   name: "profile",
   initialState,
   reducers: {
-    addToFavorite: (state, action) => {
+    addToFavorite: (state, action: PayloadAction<FavoriteItem>) => {
       const isExist = state.favoriteList.find((item) => item.id === action.payload.id);
       if(!isExist) {
         state.favoriteList.push(action.payload);
       }
     },
-    deleteFromFavorite: (state, action) => {
+    deleteFromFavorite: (state, action: PayloadAction<string>) => {
       state.favoriteList = state.favoriteList.filter((item) => item.id !== action.payload);
     }
   }
