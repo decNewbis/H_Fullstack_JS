88e998c7bfa9c929250444487afdd7899c12a5b3
decodload <decodload@gmail.com>
2024-07-24 14:37:19 +0300

    [refactor] hw_L58: [TS] component 'Search.jsx' converted to 'Search.tsx'

--- lesson_58/HW_L58/demy-app/src/components/Search/Search.tsx
+++ lesson_58/HW_L58/demy-app/src/components/Search/Search.tsx
@@ -1,11 +1,12 @@
+import {FC} from "react";
 import searchStyles from "./_search.module.scss";
 import mediaSearchStyles from "./_media-search.module.scss"
 
-export function Search() {
+export const Search: FC = () => {
   const combinedStyles = `${searchStyles.searchInput} ${mediaSearchStyles.searchInput}`;
   return (
     <form className={combinedStyles} action="">
       <input type="search" placeholder="Search..."/>
     </form>
   );
-}
+};
