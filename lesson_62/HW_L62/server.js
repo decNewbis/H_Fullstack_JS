e6698bdcecd84221358d73f994c1c2676fa4debf
decodload <decodload@gmail.com>
2024-08-03 02:51:40 +0300

    [feature] hw_L62: created funcs 'getProductById', 'getCartByUserId'; created endpoint to add product to user's cart

--- lesson_62/HW_L62/server.js
+++ lesson_62/HW_L62/server.js
@@ -13,6 +13,14 @@ const getUser = (xUserId) => {
   return users.find((user) => user.id === xUserId);
 };
 
+const getProductById = (productId) => {
+  return products.find((product) => product.id === +productId);
+};
+
+const getCartByUserId = (userId) => {
+  return carts.find((cart) => cart.userId === userId);
+};
+
 app.post(`${API_PATH}/register`, (req, res) => {
   const { email, password } = req.body;
   const newUser = {
@@ -51,11 +59,11 @@ app.get(`${API_PATH}/products/:productId`, (req, res) => {
 
   if (currentUser) {
     const { productId } = req.params;
-    const foundProductById = products.find((product) => product.id === +productId);
+    const foundProductById = getProductById(productId);
     if (foundProductById) {
       res.status(200).json(foundProductById);
     } else {
-      res.status(200).json({"error": "product not found"});
+      res.status(404).json({"message": "product not found"});
     }
   } else {
     res.status(401).json({"error": "you do not have access rights to the content"});
@@ -63,7 +71,35 @@ app.get(`${API_PATH}/products/:productId`, (req, res) => {
 });
 
 app.put(`${API_PATH}/cart/:productId`, (req, res) => {
+  const xUserId = req.header("x-user-id");
+  const currentUser = getUser(xUserId);
 
+  if (currentUser) {
+    const { productId } = req.params;
+    const foundProductById = getProductById(productId);
+    const cart = getCartByUserId(currentUser.id);
+
+    if (foundProductById) {
+      if (!cart) {
+        const products = [];
+        products.push(foundProductById);
+        const newCart = {
+          id: crypto.randomUUID(),
+          userId: currentUser.id,
+          products
+        };
+        carts.push(newCart);
+        res.status(200).json(newCart);
+      } else {
+        cart.products.push(foundProductById);
+        res.status(200).json(cart);
+      }
+    } else {
+      res.status(404).json({"message": "product not found"});
+    }
+  } else {
+    res.status(401).json({"error": "you do not have access rights to the content"});
+  }
 });
 
 app.delete(`${API_PATH}/cart/:productId`, (req, res) => {
