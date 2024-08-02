a19a191048d26fe5f98ccac025c6aafc9285de97
decodload <decodload@gmail.com>
2024-08-03 01:41:16 +0300

    [feature] hw_L62: created endpoint of user register

---
+++ lesson_62/HW_L62/server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import crypto from "crypto";
+import bodyParser from "body-parser";
+
+import { products, users, carts, orders } from "./storage.js";
+
+const app = express();
+const PORT = 3000;
+const API_PATH = '/api';
+app.use(bodyParser.json());
+
+app.post(`${API_PATH}/register`, (req, res) => {
+  const { email, password } = req.body;
+  const newUser = {
+    id: crypto.randomUUID(),
+    email,
+    password,
+  };
+
+  users.push(newUser);
+  const currentUser = users.find((user) => user.id === newUser.id);
+
+  if (currentUser) {
+    res.status(201).json({
+      id: currentUser.id,
+      email: currentUser.email
+    });
+  } else {
+    res.status(500).json({"error": "registration failed"});
+  }
+});
+
+app.get(`${API_PATH}/products`, (req, res) => {
+
+});
+
+app.get(`${API_PATH}/products/:productId`, (req, res) => {
+
+});
+
+app.put(`${API_PATH}/cart/:productId`, (req, res) => {
+
+});
+
+app.delete(`${API_PATH}/cart/:productId`, (req, res) => {
+
+});
+
+app.post(`${API_PATH}/cart/checkout`, (req, res) => {})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
