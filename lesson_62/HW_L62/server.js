e16c06c28ea78a25435424da8528ad974bc6a5e1
decodload <decodload@gmail.com>
2024-08-04 21:57:19 +0300

    [refactor] hw_L62: nested conditions are minimized, removed unnecessary conditions, created funcs 'sendProductNotFoundResponse', 'sendUnauthorizedResponse'

--- lesson_62/HW_L62/server.js
+++ lesson_62/HW_L62/server.js
@@ -25,6 +25,14 @@ const getOrderByUserId = (userId) => {
   return orders.find((order) => order.userId === userId);
 };
 
+const sendProductNotFoundResponse = (res) => {
+  return res.status(404).json({"message": "product not found"});
+};
+
+const sendUnauthorizedResponse = (res) => {
+  res.status(401).json({"error": "you do not have access rights to the content"});
+};
+
 app.post(`${API_PATH}/register`, (req, res) => {
   const { email, password } = req.body;
 
@@ -39,16 +47,11 @@ app.post(`${API_PATH}/register`, (req, res) => {
     password,
   };
   users.push(newUser);
-  const currentUser = users.find((user) => user.id === newUser.id);
 
-  if (currentUser) {
-    return res.status(201).json({
-      id: currentUser.id,
-      email: currentUser.email
-    });
-  } else {
-    return res.status(500).json({"error": "registration failed"});
-  }
+  return res.status(201).json({
+    id: newUser.id,
+    email: newUser.email
+  });
 });
 
 app.get(`${API_PATH}/products`, (req, res) => {
@@ -58,7 +61,8 @@ app.get(`${API_PATH}/products`, (req, res) => {
   if (currentUser) {
     res.status(200).json(products);
   } else {
-    res.status(401).json({"error": "you do not have access rights to the content"});
+    // res.status(401).json({"error": "you do not have access rights to the content"});
+    sendUnauthorizedResponse(res);
   }
 });
 
@@ -66,16 +70,15 @@ app.get(`${API_PATH}/products/:productId`, (req, res) => {
   const xUserId = req.header("x-user-id");
   const currentUser = getUser(xUserId);
 
-  if (currentUser) {
-    const { productId } = req.params;
-    const foundProductById = getProductById(productId);
-    if (foundProductById) {
-      res.status(200).json(foundProductById);
-    } else {
-      res.status(404).json({"message": "product not found"});
-    }
+  if (!currentUser) {
+    return sendUnauthorizedResponse(res);
+  }
+  const { productId } = req.params;
+  const foundProductById = getProductById(productId);
+  if (foundProductById) {
+    res.status(200).json(foundProductById);
   } else {
-    res.status(401).json({"error": "you do not have access rights to the content"});
+    sendProductNotFoundResponse(res);
   }
 });
 
@@ -83,31 +86,30 @@ app.put(`${API_PATH}/cart/:productId`, (req, res) => {
   const xUserId = req.header("x-user-id");
   const currentUser = getUser(xUserId);
 
-  if (currentUser) {
-    const { productId } = req.params;
-    const foundProductById = getProductById(productId);
-    const cart = getCartByUserId(currentUser.id);
+  if (!currentUser) {
+    return sendUnauthorizedResponse(res);
+  }
 
-    if (foundProductById) {
-      if (!cart) {
-        const products = [];
-        products.push(foundProductById);
-        const newCart = {
-          id: crypto.randomUUID(),
-          userId: currentUser.id,
-          products
-        };
-        carts.push(newCart);
-        res.status(200).json(newCart);
-      } else {
-        cart.products.push(foundProductById);
-        res.status(200).json(cart);
-      }
-    } else {
-      res.status(404).json({"message": "product not found"});
-    }
+  const { productId } = req.params;
+  const foundProductById = getProductById(productId);
+  const cart = getCartByUserId(currentUser.id);
+
+  if (!foundProductById) {
+    return sendProductNotFoundResponse(res);
+  }
+  if (!cart) {
+    const products = [];
+    products.push(foundProductById);
+    const newCart = {
+      id: crypto.randomUUID(),
+      userId: currentUser.id,
+      products
+    };
+    carts.push(newCart);
+    res.status(200).json(newCart);
   } else {
-    res.status(401).json({"error": "you do not have access rights to the content"});
+    cart.products.push(foundProductById);
+    res.status(200).json(cart);
   }
 });
 
@@ -115,18 +117,18 @@ app.delete(`${API_PATH}/cart/:productId`, (req, res) => {
   const xUserId = req.header("x-user-id");
   const currentUser = getUser(xUserId);
 
-  if (currentUser) {
-    const { productId } = req.params;
-    const cart = getCartByUserId(currentUser.id);
+  if (!currentUser) {
+    return sendUnauthorizedResponse(res);
+  }
 
-    if (cart) {
-      cart.products = cart.products.filter((product) => product.id !== +productId);
-      res.status(200).json(cart);
-    } else {
-      res.status(204);
-    }
+  const { productId } = req.params;
+  const cart = getCartByUserId(currentUser.id);
+
+  if (cart) {
+    cart.products = cart.products.filter((product) => product.id !== +productId);
+    res.status(200).json(cart);
   } else {
-    res.status(401).json({"error": "you do not have access rights to the content"});
+    res.status(204);
   }
 });
 
@@ -134,32 +136,32 @@ app.post(`${API_PATH}/cart/checkout`, (req, res) => {
   const xUserId = req.header("x-user-id");
   const currentUser = getUser(xUserId);
 
-  if (currentUser) {
-    const cart = getCartByUserId(currentUser.id);
-    const order = getOrderByUserId(currentUser.id);
+  if (!currentUser) {
+    return sendUnauthorizedResponse(res);
+  }
+  const cart = getCartByUserId(currentUser.id);
+  const order = getOrderByUserId(currentUser.id);
 
-    if (cart) {
-      const totalPrice = cart.products.reduce((total, product) => {
-        return total + product.price;
-      }, 0);
-      if (order) {
-        order.products = cart.products;
-        order.totalPrice = totalPrice;
-        res.status(200).json(order);
-      } else {
-        const newOrder = {
-          ...cart,
-          id: crypto.randomUUID(),
-          totalPrice
-        };
-        orders.push(newOrder);
-        res.status(200).json(newOrder);
-      }
-    } else {
-      res.status(404).json({"message": "product not found"});
-    }
+  if (!cart) {
+    return sendProductNotFoundResponse(res);
+  }
+
+  const totalPrice = cart.products.reduce((total, product) => {
+    return total + product.price;
+  }, 0);
+
+  if (order) {
+    order.products = cart.products;
+    order.totalPrice = totalPrice;
+    res.status(200).json(order);
   } else {
-    res.status(401).json({"error": "you do not have access rights to the content"});
+    const newOrder = {
+      ...cart,
+      id: crypto.randomUUID(),
+      totalPrice
+    };
+    orders.push(newOrder);
+    res.status(200).json(newOrder);
   }
 });
 
