0446479eec1abdf75289e95284e4bc4a7eaa4ba7
decodload <decodload@gmail.com>
2024-08-31 03:46:58 +0300

    [refactor] hw_L65: applied func from  'user.controllers.js'

--- lesson_65/HW_L65/routes/user.routes.js
+++ lesson_65/HW_L65/routes/user.routes.js
@@ -1,24 +1,9 @@
 import {Router} from "express";
-import {randomUUID as uuid} from "crypto";
 import {signupMiddlewareArray} from "../middlewares.js";
-import {addNewUser} from "../repositories/user.repository.js";
+import {signUp} from "../controllers/user.controllers.js";
 
 const router = Router();
-const xUserIdKey = process.env.X_USER_ID_KEY;
 
-router.post('/register', signupMiddlewareArray, (req, res) => {
-  const { email, password } = req.body;
-  const newUser = addNewUser({
-    id: uuid(),
-    email,
-    password,
-  });
-
-  res.set(xUserIdKey, newUser.id);
-  return res.status(201).json({
-    id: newUser.id,
-    email: newUser.email
-  });
-});
+router.post('/register', signupMiddlewareArray, signUp);
 
 export default router;
