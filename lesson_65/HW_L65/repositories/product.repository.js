84583aad6477d13bdeaa02bb25bff43ae9ba8321
decodload <decodload@gmail.com>
2024-08-31 04:35:49 +0300

    [refactor] hw_L65: added const '__filename', '__dirname'

--- lesson_65/HW_L65/repositories/product.repository.js
+++ lesson_65/HW_L65/repositories/product.repository.js
@@ -1,6 +1,10 @@
-import {readFile, writeFile} from "fs/promises";
 import path from "path";
+import {fileURLToPath} from "url";
+import {readFile, writeFile} from "fs/promises";
 import {ErrorReadWriteFile} from "../errorHandler.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const readProductsStore = async (filename) => {
   try {
