e377b05581dcc9c3d82b40922b2c5d326577602a
decodload <decodload@gmail.com>
2024-08-31 04:56:08 +0300

    [refactor] hw_L65: applied funcs from 'user.services.js', removed unnecessary code

--- lesson_65/HW_L65/controllers/user.controllers.js
+++ lesson_65/HW_L65/controllers/user.controllers.js
@@ -1,19 +1,10 @@
-import {randomUUID as uuid} from "crypto";
-import {addNewUser} from "../repositories/user.repository.js";
+import {createNewUser} from "../services/user.services.js";
 
 const xUserIdKey = process.env.X_USER_ID_KEY;
 
 export const signUp = (req, res) => {
-  const { email, password } = req.body;
-  const newUser = addNewUser({
-    id: uuid(),
-    email,
-    password,
-  });
+  const newUser = createNewUser(req.body);
 
   res.set(xUserIdKey, newUser.id);
-  return res.status(201).json({
-    id: newUser.id,
-    email: newUser.email
-  });
+  return res.status(201).json(newUser);
 };
