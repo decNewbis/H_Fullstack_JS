be0025e2dfeec5320e9d17d48a5e48d6b0abd35c
decodload <decodload@gmail.com>
2024-08-31 03:56:07 +0300

    [refactor] hw_L65: applied func from  'products.controllers.js'

--- lesson_65/HW_L65/routes/products.routes.js
+++ lesson_65/HW_L65/routes/products.routes.js
@@ -1,22 +1,11 @@
 import {Router} from "express";
 import {isAuthorized} from "../middlewares.js";
-import {ErrorObjectNotFound} from "../errorHandler.js";
-import {getProducts, getProductById} from "../repositories/products.repository.js";
+import {fetchProducts, fetchProductById} from "../controllers/products.controllers.js";
 
 const router = Router();
 
-router.get('/', isAuthorized, (req, res) => {
-  res.status(200).json(getProducts());
-});
+router.get('/', isAuthorized, fetchProducts);
 
-router.get('/:productId', isAuthorized, (req, res) => {
-  const { productId } = req.params;
-  const foundProductById = getProductById(productId);
-
-  if (!foundProductById) {
-    throw new ErrorObjectNotFound("product not found");
-  }
-  res.status(200).json(foundProductById);
-});
+router.get('/:productId', isAuthorized, fetchProductById);
 
 export default router;
