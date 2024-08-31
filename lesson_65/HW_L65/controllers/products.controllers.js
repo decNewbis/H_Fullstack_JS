fe1047edbb1d633c33957747f33e712a7562cd6f
decodload <decodload@gmail.com>
2024-08-31 05:03:05 +0300

    [refactor] hw_L65: applied funcs from 'products.services.js', removed unnecessary code

--- lesson_65/HW_L65/controllers/products.controllers.js
+++ lesson_65/HW_L65/controllers/products.controllers.js
@@ -1,13 +1,13 @@
-import {getProductById, getProducts} from "../repositories/products.repository.js";
+import {getProducts} from "../repositories/products.repository.js";
 import {ErrorObjectNotFound} from "../errorHandler.js";
+import {findProductById} from "../services/products.services.js";
 
 export const fetchProducts = (req, res) => {
   res.status(200).json(getProducts());
 };
 
 export const fetchProductById = (req, res) => {
-  const { productId } = req.params;
-  const foundProductById = getProductById(productId);
+  const foundProductById = findProductById(req.params);
 
   if (!foundProductById) {
     throw new ErrorObjectNotFound("product not found");
