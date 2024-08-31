1ad5448f61f648d2a00de1f5c12560c532547329
decodload <decodload@gmail.com>
2024-08-31 04:04:47 +0300

    [refactor] hw_L65: applied func from  'cart.controllers.js'

--- lesson_65/HW_L65/routes/cart.routes.js
+++ lesson_65/HW_L65/routes/cart.routes.js
@@ -1,79 +1,13 @@
 import {Router} from 'express';
-import {randomUUID as uuid} from "crypto";
 import {isAuthorized} from "../middlewares.js";
-import {ErrorObjectNotFound} from "../errorHandler.js";
-import {getUser} from "../repositories/user.repository.js";
-import {getProductById} from "../repositories/products.repository.js";
-import {getCartByUserId, getOrderByUserId, createNewCart, addNewOrder} from "../repositories/cart.repository.js";
+import {addProductByIdToCart, checkoutOrder, removeProductByIdFromCart} from "../controllers/cart.controllers.js";
 
 const router = Router();
-const xUserIdKey = process.env.X_USER_ID_KEY;
 
-router.put('/:productId', isAuthorized, (req, res) => {
-  const { productId } = req.params;
-  const foundProductById = getProductById(productId);
+router.put('/:productId', isAuthorized, addProductByIdToCart);
 
-  if (!foundProductById) {
-    throw new ErrorObjectNotFound("product not found");
-  }
+router.delete('/:productId', isAuthorized, removeProductByIdFromCart);
 
-  const xUserId = req.header(xUserIdKey);
-  const currentUser = getUser(xUserId);
-  const cart = getCartByUserId(currentUser.id);
-
-  if (!cart) {
-    const products = [];
-    products.push(foundProductById);
-    const newCart = createNewCart({
-      id: uuid(),
-      userId: currentUser.id,
-      products
-    });
-    res.status(200).json(newCart);
-  } else {
-    cart.products.push(foundProductById);
-    res.status(200).json(cart);
-  }
-});
-
-router.delete('/:productId', isAuthorized, (req, res) => {
-  const xUserId = req.header(xUserIdKey);
-  const currentUser = getUser(xUserId);
-  const { productId } = req.params;
-  const cart = getCartByUserId(currentUser.id);
-
-  if (cart) {
-    cart.products = cart.products.filter((product) => product.id !== +productId);
-    res.status(200).json(cart);
-  }
-});
-
-router.post('/checkout', isAuthorized, (req, res) => {
-  const xUserId = req.header(xUserIdKey);
-  const currentUser = getUser(xUserId);
-  const cart = getCartByUserId(currentUser.id);
-  const order = getOrderByUserId(currentUser.id);
-
-  if (!cart) {
-    throw new ErrorObjectNotFound("cart not found");
-  }
-
-  const totalPrice = cart.products.reduce((total, product) => {
-    return total + product.price;
-  }, 0);
-
-  if (order) {
-    order.products = cart.products;
-    order.totalPrice = totalPrice;
-    res.status(200).json(order);
-  } else {
-    const newOrder = addNewOrder({
-      ...cart,
-      id: uuid(),
-      totalPrice
-    });
-    res.status(200).json(newOrder);
-  }
-});
+router.post('/checkout', isAuthorized, checkoutOrder);
 
 export default router;
