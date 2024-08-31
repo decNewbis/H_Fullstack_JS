bc1a8ea0a8ce0ff001f57a69826f6afe686544e1
decodload <decodload@gmail.com>
2024-08-31 05:02:08 +0300

    [feature] hw_L65: created 'products.services.js'

---
+++ lesson_65/HW_L65/services/product.services.js
@@ -0,0 +1,79 @@
+import {createReadStream, createWriteStream, existsSync, mkdirSync} from "fs";
+import {ErrorObjectNotFound, ErrorReadWriteFile} from "../errorHandler.js";
+import eventEmitter from "../eventEmits.js";
+import {readProductsStore, writeProductsStore} from "../repositories/product.repository.js";
+import sharp from "sharp";
+
+const productsStore = process.env.PRODUCTS_STORE;
+
+export const getCustomProductById = (productId, productsList) => {
+  return productsList.find((product) => product.id === `${productId}`);
+};
+
+export const ensureFileExists = (filename) => {
+  if (!existsSync(filename)) {
+    throw new ErrorObjectNotFound('File not found');
+  }
+};
+
+export const ensureDirectoryExists = (directory) => {
+  if (!existsSync(directory)) {
+    mkdirSync(directory);
+  }
+};
+
+export const handleFileUpload = async (req, res, next, uploadParams) => {
+  const {
+    productId,
+    filename,
+    previewFilename=null,
+    filePath,
+    previewFilePath=null,
+    fileType
+  } = uploadParams;
+
+  eventEmitter.emit('fileUploadStart', {productId, filename});
+  const writeableStream = createWriteStream(filePath, { encoding: "binary", flags: "w" });
+  req.pipe(writeableStream)
+    .on('finish', async () => {
+      try {
+        const customProductsList = await readProductsStore(productsStore);
+        const foundProduct = getCustomProductById(productId, customProductsList);
+        foundProduct[fileType].push(filename);
+        if (fileType === 'images') {
+          foundProduct.previews.push(previewFilename)
+        }
+
+        await writeProductsStore(productsStore, customProductsList);
+
+        if (fileType === 'images') {
+          await sharp(filePath)
+            .resize(150, 150)
+            .toFile(previewFilePath);
+        }
+        res.status(200).send(foundProduct);
+        eventEmitter.emit('fileUploadEnd', {productId, filename});
+      } catch (err) {
+        eventEmitter.emit('fileUploadFailed', {productId, filename, err});
+        next(new ErrorReadWriteFile(err));
+      }
+    })
+    .on('error', (err) => {
+      eventEmitter.emit('fileUploadFailed', {productId, filename, err});
+      next(new ErrorReadWriteFile(err));
+    });
+};
+
+export const getFileByName = (res, next, requestParams) => {
+  const {filePath, contentType} = requestParams;
+  try {
+    ensureFileExists(filePath, next);
+  } catch (err) {
+    return next(err);
+  }
+  const head = {
+    "Content-Type": contentType
+  };
+  res.writeHead(200, head);
+  createReadStream(filePath).pipe(res);
+};
