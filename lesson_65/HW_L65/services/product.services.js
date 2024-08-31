6e9ffdbb424223ca8a825bdc03eab5bbfe93634c
decodload <decodload@gmail.com>
2024-08-31 19:10:27 +0300

    [refactor] hw_L65: created funcs  'product.services.js'

--- lesson_65/HW_L65/services/product.services.js
+++ lesson_65/HW_L65/services/product.services.js
@@ -1,79 +1,117 @@
-import {createReadStream, createWriteStream, existsSync, mkdirSync} from "fs";
-import {ErrorObjectNotFound, ErrorReadWriteFile} from "../errorHandler.js";
-import eventEmitter from "../eventEmits.js";
+import {randomUUID as uuid} from "crypto";
+import path from "path";
+import {fileURLToPath} from "url";
 import {readProductsStore, writeProductsStore} from "../repositories/product.repository.js";
-import sharp from "sharp";
+import {ensureDirectoryExists, handleFileUpload, getFileByName} from "../utils/file.utils.js";
 
 const productsStore = process.env.PRODUCTS_STORE;
+const productImgFormat = process.env.PRODUCT_IMG_FORMAT;
+const productVideoFormat = process.env.PRODUCT_VIDEO_FORMAT;
+const imgFolderName = process.env.IMG_FOLDER_NAME;
+const videosFolderName = process.env.VIDEOS_FOLDER_NAME;
+const previewFolderName = process.env.PREVIEWS_FOLDER_NAME;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const imgFolderNamePath = path.join(__dirname, '../', imgFolderName);
+const previewFolderNamePath = path.join(__dirname, '../', previewFolderName);
+const videosFolderNamePath = path.join(__dirname, '../', videosFolderName);
 
 export const getCustomProductById = (productId, productsList) => {
   return productsList.find((product) => product.id === `${productId}`);
 };
 
-export const ensureFileExists = (filename) => {
-  if (!existsSync(filename)) {
-    throw new ErrorObjectNotFound('File not found');
+export const createNewProduct = async ({name, description, price}) => {
+  const newProduct = {
+    id: uuid(),
+    name,
+    description,
+    price,
+    videos: [],
+    images: [],
+    previews: []
   }
+
+  const customProductsList = await readProductsStore(productsStore);
+  customProductsList.push(newProduct);
+  await writeProductsStore(productsStore, customProductsList);
+
+  return newProduct;
 };
 
-export const ensureDirectoryExists = (directory) => {
-  if (!existsSync(directory)) {
-    mkdirSync(directory);
-  }
-};
-
-export const handleFileUpload = async (req, res, next, uploadParams) => {
-  const {
+export const uploadNewImage = async (req, res, next) => {
+  const filename = `${uuid()}.${productImgFormat}`;
+  const previewFilename = `resized_${filename}`;
+  const { productId } = req.params;
+  const imageFilePath = path.join(imgFolderNamePath, filename);
+  const previewImageFilePath = path.join(previewFolderNamePath, previewFilename);
+  const fileType = 'images';
+  const uploadParams = {
     productId,
     filename,
-    previewFilename=null,
-    filePath,
-    previewFilePath=null,
+    previewFilename,
+    filePath: imageFilePath,
+    previewFilePath: previewImageFilePath,
     fileType
-  } = uploadParams;
+  };
 
-  eventEmitter.emit('fileUploadStart', {productId, filename});
-  const writeableStream = createWriteStream(filePath, { encoding: "binary", flags: "w" });
-  req.pipe(writeableStream)
-    .on('finish', async () => {
-      try {
-        const customProductsList = await readProductsStore(productsStore);
-        const foundProduct = getCustomProductById(productId, customProductsList);
-        foundProduct[fileType].push(filename);
-        if (fileType === 'images') {
-          foundProduct.previews.push(previewFilename)
-        }
+  ensureDirectoryExists(imgFolderNamePath);
+  ensureDirectoryExists(previewFolderNamePath);
 
-        await writeProductsStore(productsStore, customProductsList);
-
-        if (fileType === 'images') {
-          await sharp(filePath)
-            .resize(150, 150)
-            .toFile(previewFilePath);
-        }
-        res.status(200).send(foundProduct);
-        eventEmitter.emit('fileUploadEnd', {productId, filename});
-      } catch (err) {
-        eventEmitter.emit('fileUploadFailed', {productId, filename, err});
-        next(new ErrorReadWriteFile(err));
-      }
-    })
-    .on('error', (err) => {
-      eventEmitter.emit('fileUploadFailed', {productId, filename, err});
-      next(new ErrorReadWriteFile(err));
-    });
+  await handleFileUpload(req, res, next, uploadParams);
 };
 
-export const getFileByName = (res, next, requestParams) => {
-  const {filePath, contentType} = requestParams;
-  try {
-    ensureFileExists(filePath, next);
-  } catch (err) {
-    return next(err);
-  }
-  const head = {
-    "Content-Type": contentType
+export const uploadNewVideo = async (req, res, next) => {
+  const filename = `${uuid()}.${productVideoFormat}`;
+  const { productId } = req.params;
+  const videoFilePath = path.join(videosFolderNamePath, filename);
+  const fileType = 'videos';
+  const uploadParams = {
+    productId,
+    filename,
+    filePath: videoFilePath,
+    fileType
   };
-  res.writeHead(200, head);
-  createReadStream(filePath).pipe(res);
+
+  ensureDirectoryExists(videosFolderNamePath);
+
+  await handleFileUpload(req, res, next, uploadParams);
+};
+
+export const retrieveProductImage = (req, res, next) => {
+  const { filename } = req.params;
+  const filePath = path.join(imgFolderNamePath, filename);
+  const contentType = `image/${productImgFormat}`;
+
+  const requestParams = {
+    filePath,
+    contentType
+  };
+
+  getFileByName(res, next, requestParams);
+};
+
+export const retrieveProductVideo = (req, res, next) => {
+  const { filename } = req.params;
+  const filePath = path.join(videosFolderNamePath, filename);
+  const contentType = `video/${productVideoFormat}`;
+
+  const requestParams = {
+    filePath,
+    contentType
+  };
+
+  getFileByName(res, next, requestParams);
+};
+
+export const retrieveProductPreview = (req, res, next) => {
+  const { filename } = req.params;
+  const filePath = path.join(previewFolderNamePath, filename);
+  const contentType = `image/${productImgFormat}`;
+
+  const requestParams = {
+    filePath,
+    contentType
+  };
+
+  getFileByName(res, next, requestParams);
 };
