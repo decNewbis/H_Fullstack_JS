25f07ed6626d1a7daf58399463547611d2be6a8a
decodload <decodload@gmail.com>
2024-09-01 23:58:10 +0300

    [refactor] hw_L65: 'product.controllers.js' - created 'handleResponseUploadMedia' and 'handleResponseGetMedia'

--- lesson_65/HW_L65/controllers/product.controllers.js
+++ lesson_65/HW_L65/controllers/product.controllers.js
@@ -17,22 +17,36 @@ export const addProduct = async (req, res, next) => {
   }
 };
 
+const handleResponseUploadMedia = (res, next) => (err, product) => {
+  if (err) {
+    return next(new ErrorReadWriteFile(err));
+  }
+  res.status(200).send(product);
+};
+
+const handleResponseGetMedia = (res, next) => (err, head) => {
+  if (err) {
+    return next(new ErrorReadWriteFile(err));
+  }
+  res.writeHead(200, head);
+};
+
 export const productImageUpload = async (req, res, next) => {
-  await uploadNewImage(req, res, next);
+  await uploadNewImage(req, res, next, handleResponseUploadMedia(res, next));
 };
 
 export const productVideoUpload = async (req, res, next) => {
-  await uploadNewVideo(req, res, next);
+  await uploadNewVideo(req, res, next, handleResponseUploadMedia(res, next));
 };
 
 export const getProductImageByName = (req, res, next) => {
-  retrieveProductImage(req, res, next);
+  retrieveProductImage(req, res, next, handleResponseGetMedia(res, next));
 };
 
 export const getProductVideoByName = (req, res, next) => {
-  retrieveProductVideo(req, res, next);
+  retrieveProductVideo(req, res, next, handleResponseGetMedia(res, next));
 };
 
 export const getProductPreviewByName = (req, res, next) => {
-  retrieveProductPreview(req, res, next);
+  retrieveProductPreview(req, res, next, handleResponseGetMedia(res, next));
 };
