1fd9bf7ad8e45e340856dcae95e15d7ac218a739
decodload <decodload@gmail.com>
2024-09-07 18:41:51 +0300

    [feature] hw_L68: created endpoints: 'login', 'logout'

--- lesson_68/HW_L68/routes/user.routes.js
+++ lesson_68/HW_L68/routes/user.routes.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
 import {signupMiddlewareArray} from "../middlewares.js";
-import {signUp} from "../controllers/user.controllers.js";
+import {signUp, logIn, logOut} from "../controllers/user.controllers.js";
 
 const router = Router();
 
 router.post('/register', signupMiddlewareArray, signUp);
+router.post('/login', logIn);
+router.post('/logout', logOut);
 
 export default router;
