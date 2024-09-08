69ede115cbbb1083dd39d0edf5bb9ddeb895af57
decodload <decodload@gmail.com>
2024-09-08 05:45:02 +0300

    [refactor] hw_L68: applied check role into routes; added endpoint 'tokens' with renewal tokens

--- lesson_68/HW_L68/routes/user.routes.js
+++ lesson_68/HW_L68/routes/user.routes.js
@@ -1,11 +1,14 @@
 import {Router} from "express";
-import {signupMiddlewareArray} from "../middlewares.js";
-import {signUp, logIn, logOut} from "../controllers/user.controllers.js";
+import {isAuthorized, signupMiddlewareArray} from "../middlewares.js";
+import {signUp, logIn, logOut, renewalTokens} from "../controllers/user.controllers.js";
+import {roles} from "../roles.js";
 
 const router = Router();
+const allRoles = [roles.ADMIN, roles.CUSTOMER];
 
 router.post('/register', signupMiddlewareArray, signUp);
 router.post('/login', logIn);
 router.post('/logout', logOut);
+router.post('/tokens', isAuthorized(allRoles), renewalTokens);
 
 export default router;
