e6abb6a7b632e9efcf2c835da47f75824b792b87
decodload <decodload@gmail.com>
2024-09-20 04:08:40 +0300

    [feature] hw_L71: created model 'product', created 'productSchema'

---
+++ lesson_71/HW_L71/models/product.js
@@ -0,0 +1,56 @@
+import mongoose from  'mongoose';
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    max: 50
+  },
+  description: {
+    type: String,
+    required: true,
+    max: 256
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  videos: {
+    type: Array,
+    default: [],
+    items: {
+      type: String,
+    },
+    max: 5
+  },
+  images: {
+    type: Array,
+    default: [],
+    items: {
+      type: String,
+    },
+    max: 10
+  },
+  previews: {
+    type: Array,
+    default: [],
+    items: {
+      type: String,
+    },
+    max: 10
+  }
+});
+
+productSchema.path('videos').validate((videosList) => {
+  return videosList.length <= 5;
+}, 'Maximum 5 videos');
+productSchema.path('images').validate((imagesList) => {
+  return imagesList.length <= 10;
+}, 'Maximum 10 images');
+productSchema.path('previews').validate((previewsList) => {
+  return previewsList.length <= 10;
+}, 'Maximum 10 previews');
+
+export const Product = mongoose.model('Product', productSchema);
