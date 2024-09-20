a243083382753f5370c867da99f0febec6b9bf05
decodload <decodload@gmail.com>
2024-09-20 19:07:44 +0300

    [refactor] hw_L71: created product repository funcs: 'addAndSaveNewProduct', 'addAndSaveNewVideo', 'addAndSaveNewImage', 'addAndSaveNewPreview', 'findByIdAndUpdate'

--- lesson_71/HW_L71/repositories/product.repository.js
+++ lesson_71/HW_L71/repositories/product.repository.js
@@ -2,6 +2,10 @@ import path from "path";
 import {fileURLToPath} from "url";
 import {readFile, writeFile} from "fs/promises";
 import {ErrorReadWriteFile} from "../errorHandler.js";
+import {Video} from "../models/video.js";
+import {Image} from "../models/image.js";
+import {Preview} from "../models/preview.js";
+import {Product} from "../models/product.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -21,4 +25,32 @@ export const writeProductsStore = async (filename, data) => {
   } catch (err) {
     throw new ErrorReadWriteFile(err);
   }
+};
+
+export const addAndSaveNewProduct = async (data) => {
+  return await new Product(data).save();
+};
+
+export const addAndSaveNewVideo = async (filename) => {
+  return await new Video({name: `${filename}`}).save();
+};
+
+export const addAndSaveNewImage = async (filename) => {
+  return await new Image({name: `${filename}`}).save();
+};
+
+export const addAndSaveNewPreview = async (filename) => {
+  return await new Preview({name: `${filename}`}).save();
+};
+
+export const findByIdAndUpdate = async (productId, update) => {
+  const options = { new: true };
+
+  return Product
+    .findByIdAndUpdate(
+      productId,
+      update,
+      options
+    )
+    .populate('videos images previews', '_id name');
 };
