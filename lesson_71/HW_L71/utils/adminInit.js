1df8a568bbaacbe0804c51c737e9e64c520f40c4
decodload <decodload@gmail.com>
2024-09-21 23:28:41 +0300

    [refactor] hw_L71: check is initial admin already exists in DB

--- lesson_71/HW_L71/utils/adminInit.js
+++ lesson_71/HW_L71/utils/adminInit.js
@@ -1,10 +1,16 @@
 import {createNewUser} from "../services/user.services.js";
 import {roles} from "../roles.js";
+import {getUserByEmail} from "../repositories/user.repository.js";
+import {ErrorUserAlreadyExists} from "../errorHandler.js";
 
 export const initializeAdmin = async () => {
   const adminParams = {
     email: process.env.SUPER_USER_EMAIL,
     password: process.env.SUPER_USER_PASSWORD
   };
+  const isFoundUser = await getUserByEmail(adminParams.email);
+  if (isFoundUser) {
+    return;
+  }
   await createNewUser(adminParams, roles.ADMIN);
 };
