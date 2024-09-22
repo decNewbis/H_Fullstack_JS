a1b01b01f373c3698d2f66094ea24383df1f5471
decodload <decodload@gmail.com>
2024-09-22 04:04:44 +0300

    [refactor] hw_L71: replaced endpoints from 'products' to 'product': '/product/all', '/product/:productId'

--- lesson_71/HW_L71/routes/product.routes.js
+++ lesson_71/HW_L71/routes/product.routes.js
@@ -9,12 +9,15 @@ import {
   productVideoUpload
 } from "../controllers/product.controllers.js";
 import {roles} from "../roles.js";
+import {fetchProductById, fetchProducts} from "../controllers/product.controllers.js";
 
 const router = Router();
 const admin  = [roles.ADMIN];
 const allRoles = [roles.ADMIN, roles.CUSTOMER];
 
 router.post('/', isAuthorized(admin), addProduct);
+router.get('/all', isAuthorized(allRoles), fetchProducts);
+router.get('/:productId', isAuthorized(allRoles), fetchProductById);
 router.post('/:productId/image/upload', isAuthorized(admin), productImageUpload);
 router.post('/:productId/video/upload', isAuthorized(admin), productVideoUpload);
 router.get('/image/:filename', isAuthorized(allRoles), getProductImageByName);
