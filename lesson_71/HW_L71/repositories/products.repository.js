dd4ef5d5359efa1185bd0efad58ec39376d88455
decodload <decodload@gmail.com>
2024-09-22 04:21:45 +0300

    [refactor] hw_L71: removed unnecessary repository 'products'

--- lesson_71/HW_L71/repositories/products.repository.js
+++
@@ -1,9 +0,0 @@
-import {products} from "../storage.js";
-
-export const getProducts = () => {
-  return products;
-};
-
-export const getProductById = (productId) => {
-  return products.find((product) => product.id === +productId);
-};
