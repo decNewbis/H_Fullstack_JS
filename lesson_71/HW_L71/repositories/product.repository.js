daca8ca875fa583097cf85ab8e777348dd5854dc
decodload <decodload@gmail.com>
2024-09-22 04:12:26 +0300

    [refactor] hw_L71: removed unnecessary code; made async and replaced func 'getProducts', 'getProductById' from 'products' repository to 'product' repository

--- lesson_71/HW_L71/repositories/product.repository.js
+++ lesson_71/HW_L71/repositories/product.repository.js
@@ -1,31 +1,7 @@
-import path from "path";
-import {fileURLToPath} from "url";
-import {readFile, writeFile} from "fs/promises";
-import {ErrorReadWriteFile} from "../errorHandler.js";
 import {Video} from "../models/video.js";
 import {Image} from "../models/image.js";
 import {Preview} from "../models/preview.js";
 import {Product} from "../models/product.js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export const readProductsStore = async (filename) => {
-  try {
-    const data = await readFile(path.join(__dirname, '../', filename), { encoding: "utf8" });
-    return JSON.parse(data);
-  } catch (err) {
-    throw new ErrorReadWriteFile(err);
-  }
-};
-
-export const writeProductsStore = async (filename, data) => {
-  try {
-    await writeFile(path.join(__dirname, '../', filename), JSON.stringify(data), { encoding: "utf8", flag: "w" });
-  } catch (err) {
-    throw new ErrorReadWriteFile(err);
-  }
-};
 
 export const addAndSaveNewProduct = async (data) => {
   return await new Product(data).save();
@@ -53,4 +29,12 @@ export const findByIdAndUpdate = async (productId, update) => {
       options
     )
     .populate('videos images previews', '_id name');
+};
+
+export const getProducts = async () => {
+  return Product.find();
+};
+
+export const getProductById = async (productId) => {
+  return Product.findById(productId);
 };
