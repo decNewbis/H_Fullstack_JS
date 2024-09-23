20b202d1edfe6c972fb3c260710dc62846c791a8
decodload <decodload@gmail.com>
2024-09-23 15:01:35 +0300

    [refactor] hw_L71: created func 'findByUserIdAndUpdate' in cart repository

--- lesson_71/HW_L71/repositories/cart.repository.js
+++ lesson_71/HW_L71/repositories/cart.repository.js
@@ -1,7 +1,20 @@
 import {carts, orders} from "../storage.js";
+import {Cart} from "../models/cart.js";
 
-export const getCartByUserId = (userId) => {
+export const getCartByUserId = async (userId) => {
   return carts.find((cart) => cart.userId === userId);
+};
+
+export const findByUserIdAndUpdate = async (userId, update) => {
+  const options = {
+    new: true,
+    upsert: true
+  }
+  return Cart.findOneAndUpdate(
+    {userId},
+    update,
+    options
+  ).populate('products', '_id name description price');
 };
 
 export const getOrderByUserId = (userId) => {
