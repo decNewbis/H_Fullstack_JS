d056f9f81d03ec6b316408ec067818b468cdd377
decodload <decodload@gmail.com>
2024-09-23 18:24:15 +0300

    [feature] hw_L71: created func 'findOrderByUserIdAndUpdate', 'getProductsFromCartByUserId', applied mongodb (cart repository)

--- lesson_71/HW_L71/repositories/cart.repository.js
+++ lesson_71/HW_L71/repositories/cart.repository.js
@@ -1,11 +1,11 @@
-import {carts, orders} from "../storage.js";
 import {Cart} from "../models/cart.js";
+import {Order} from "../models/order.js";
 
-export const getCartByUserId = async (userId) => {
-  return carts.find((cart) => cart.userId === userId);
+export const getProductsFromCartByUserId = async (userId) => {
+  return Cart.findOne({userId}).populate('products', '_id name price');
 };
 
-export const findByUserIdAndUpdate = async (userId, update) => {
+export const findCartByUserIdAndUpdate = async (userId, update) => {
   const options = {
     new: true,
     upsert: true
@@ -17,16 +17,16 @@ export const findByUserIdAndUpdate = async (userId, update) => {
   ).populate('products', '_id name description price');
 };
 
-export const getOrderByUserId = (userId) => {
-  return orders.find((order) => order.userId === userId);
-};
+export const findOrderByUserIdAndUpdate = async (userId, update) => {
+  const options = {
+    new: true,
+    upsert: true
+  }
 
-export const createNewCart = (newCart) => {
-  carts.push(newCart);
-  return newCart;
-};
-
-export const addNewOrder = (newOrder) => {
-  orders.push(newOrder);
-  return newOrder;
+  return Order.findOneAndUpdate(
+    {userId},
+    update,
+    options
+  )
+    .populate('userId', '_id email');
 };
