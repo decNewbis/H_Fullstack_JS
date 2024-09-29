9cd67ed5d9904997fba4234e4a7d13069ff6951c
decodload <decodload@gmail.com>
2024-09-30 02:55:33 +0300

    [refactor] hw_L71: added match with regexp to userSchema email and password

--- lesson_71/HW_L71/models/user.js
+++ lesson_71/HW_L71/models/user.js
@@ -7,11 +7,13 @@ const userSchema = new Schema({
     type: String,
     required: true,
     max: 254,
-    unique: true
+    unique: true,
+    match: [new RegExp(/^(([a-zA-Z0-9._-]+)@([a-zA-Z0-9.-]+)(\.[a-zA-Z]{2,}))/, "iu"), 'Please provide a valid email address']
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    match: [new RegExp(/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]/), 'Please provide a valid password']
   },
   role: {
     type: String,
